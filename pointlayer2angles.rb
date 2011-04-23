0596274dcff643e1d84bfcccf4c31531a47a83a9
Casey Dreier <cdreier@gmail.com>
2011-04-23 00:55:12 -0400

    Adding execution code.

--- pointlayer2angles.rb
+++ pointlayer2angles.rb
@@ -1,3 +1,4 @@
+
 # Given a text file with the following structure:
 #  OBJECTID1,ORIG_FID1,POINT_X,POINT_Y
 #  OBJECTID2,ORIG_FID1,POINT_X,POINT_Y
@@ -11,6 +12,16 @@
 #   pointlayer2angles point_layer_file.txt > angle_file.txt
 #
 
+require 'rdoc/usage'
+def print_usage
+  puts "Usage: \t#{$0} points_layer_file.txt > angle_file.txt" #$0 is the name of this file
+end
+
+
+# Make sure we have proper number of args for this file #
+if ARGV.size < 1
+  RDoc::usage(1)
+end
 
 module PointLayer
 
@@ -154,9 +165,14 @@ module PointLayer
     
   end
 
-  class Statistics
-    def initializer()
-      
-    end
-  end
+end
+
+points_file = ARGV[0]
+
+# Read the File and output Line Data to STDOUT
+points_data = PointLayer::FileReader.new(:file => points_file)
+puts "ORIG_FID,POINT_X1,POINT_Y1,POINT_X2,POINT_X2,ANGLE"
+points_data.each do |line_data|
+  line = PointLayer::Line.new(PointLayer::Point.new(line_data[:x1], line_data[:y1]), PointLayer::Point.new(line_data[:x2], line_data[:y2]))
+  puts "#{line_data[:group]},#{line_data[:x1]},#{line_data[:x2]},#{line_data[:y2]},#{line.angle_to_vertical}"
 end
